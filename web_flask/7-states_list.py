61ce028dfcd3cda00eb74ed9de943f8e47bc1918
Michaelsadek <michealsadek849@gmail.com>
2024-03-22 17:22:22 -0700

    a

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-""" 7. Start flask service that does something. """
-
-from flask import Flask
-from flask import render_template
-
-
-app = Flask(__name__)
+"""Flask app to generate html list of all states from storage"""
+from flask import Flask, render_template
+from models import storage
+app = Flask('web_flask')
 app.url_map.strict_slashes = False
 
 
-#@app.route()
+@app.route('/states_list')
+def list_of_states():
+    """Render html with unordered list of states from `storage`"""
+    states = sorted(storage.all('State').values(), key=lambda s: s.name)
+    return render_template('7-states_list.html', states=states)
 
 
-if __name__ == '__main__':
+@app.teardown_appcontext
+def teardown_db(*args, **kwargs):
+    """Close database or file storage"""
+    storage.close()
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
